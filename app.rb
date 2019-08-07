0fd40e2552ec597d10398a5b9a44e5bbf37151f0
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-07 23:27:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Melissa"
+
+  end
+
+  get '/hometown' do
+    "My hometown is Huntington Beach"
+
+  end
+
+  get '/favorite-song' do
+    "My favorite song is ?"
+
+  end
 end
